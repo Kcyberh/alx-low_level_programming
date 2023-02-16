7ee4986388457fb78024b47e7805616aea80b37d
Kcyberh <kcyberh30@gmail.com>
2023-02-16 13:58:31 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -a program that prints a line with printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
