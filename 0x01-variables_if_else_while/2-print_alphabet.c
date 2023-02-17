3e10f1c2edb99513e4dde4a97d379c8d57b492b1
Kcyberh <kcyberh30@gmail.com>
2023-02-17 07:19:37 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase with putchar
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
