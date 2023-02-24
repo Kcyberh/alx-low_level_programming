ce0d0f1438aff4ddfb4303283dfadfa86dc2a3d9
Kcyberh <kcyberh30@gmail.com>
2023-02-23 16:17:10 -0800

    102-print_comb4&5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,6 @@ int main(void)
 
 	for (p = d + 1; p <= '9'; p++)
 	{
-	
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != d) != q)
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,6 @@ int main(void)
 
 	for (p = 0; p <= 98; p++)
 	{
-		
 	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
