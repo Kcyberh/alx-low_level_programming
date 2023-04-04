1c490802b802dc3774469c56a075a3440ffc243c
Kcyberh <kcyberh30@gmail.com>
2023-04-04 02:11:22 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,45 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index -  inserts a new node at a given position.
- * @head: double pointer to the first node in the list
- * @idx: index of the node to insert
- * @n: value for new node
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: pointer to the indexed node
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i;
-listint_t *current, *new;
+ unsigned int i;
+ listint_t *new;
+ listint_t *temp = *head;
 
-if (head == NULL)
-return (NULL);
-if (idx != 0)
-{
-current = *head;
-for (i = 0; i < idx - 1 && current != NULL; i++)
-{
-current = current->next;
-}
-if (current == NULL)
-return (NULL);
-}
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->n = n;
-if (idx == 0)
-{
-new->next = *head;
-*head = new;
-return (new);
-}
-new->next = current->next;
-current->next = new;
-return (new);
+ new = malloc(sizeof(listint_t));
+ if (!new || !head)
+ return (NULL);
+
+ new->n = n;
+ new->next = NULL;
+
+ if (idx == 0)
+ {
+ new->next = *head;
+ *head = new;
+ return (new);
+ }
+
+ for (i = 0; temp && i < idx; i++)
+ {
+ if (i == idx - 1)
+ {
+ new->next = temp->next;
+ temp->next = new;
+ return (new);
+ }
+ else
+ temp = temp->next;
+ }
+
+ return (NULL);
 }
